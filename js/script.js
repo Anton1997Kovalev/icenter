c7547d5899f6f87b1b2749aa98e25cc562a00ef9
Anton Kovalev <kowalow1997@gmail.com>
2023-03-07 23:43:19 +0600

    made header desktop

--- js/script.js
+++ js/script.js
@@ -1,79 +1,83 @@
-/* ------------------------- main-setting --------------------------- */
-gsap.registerPlugin(ScrollTrigger);
-$('.burger-menu__box').on('click', function () {
-	$(this).parent().toggleClass('_active');
-})
-/* ------------------------- main-setting END --------------------------- */
+document.addEventListener('DOMContentLoaded', function(){
+	function tooltip(){
+		let $tooltip = document.querySelectorAll('.tooltip');
+		$tooltip.forEach(function($item, indx){
+			$item.addEventListener('click', function(e){
+				let $this = e.target;
+				if ($this.classList.contains('toggle-tooltip__js')){
+					getSiblings($item).forEach($i => $i.classList.remove('_active'));
+					$this.closest('.tooltip').classList.toggle('_active');
+				}
+				if ($this.classList.contains('close-tooltip__js')){
+					$this.closest('.tooltip').classList.remove('_active');
+				}
+				if ($this.classList.contains('open-list-cities__js')){
+					$this.closest('.tooltip').classList.remove('_active');
+					console.log('list-cities')
+				}
+			})
+		})
+	}
+	function submenu(){
+		let $submenu = document.querySelectorAll('.submenu__js>a');
+		$submenu.forEach(function($item, indx) {
+			$item.addEventListener('click', function(e){
+				$item.parentElement.classList.toggle('_active');
+				e.preventDefault();
+			})
+		})
+	}
+	function menuBody() {
+		let $links = document.querySelectorAll('.open-menu-body__js');
+		$links.forEach(function($link, indx){
+			$link.addEventListener('click', function(e){
+				getSiblings($link.parentElement).forEach(function($i, indx){
+					$i.querySelector('a').classList.remove('_active')
+					document.querySelector('.menu-body__category[data-category-id="'+ $i.querySelector('a').getAttribute('data-category-name') + '"]').classList.remove('_active');
+				});
+				$link.classList.toggle('_active');
+				document.querySelector('.menu-body__category[data-category-id="'+ $link.getAttribute('data-category-name') + '"]').classList.add('_active');
+				if ($link.classList.contains('_active')){
+					document.querySelector('.menu-body').classList.add('_active');
+					document.querySelector('.menu-body').style.height = document.querySelector('.menu-body .menu-body__container').clientHeight + 'px';
 
-function funcScroll() {
-	let scrollPos = 0;
-	$(window).scroll(function () {
-		let st = $(this).scrollTop();
-		if (st > scrollPos + 10) {
-			console.log('Down!');
-		} else if (st < scrollPos - 10) {
-			console.log('Up!');
+				} else{
+					document.querySelector('.menu-body').classList.remove('_active');
+					document.querySelector('.menu-body').style.height = 0;
+				}
+				e.preventDefault();
+			})
+		})
+	}
+
+	tooltip();
+	submenu();
+	menuBody();
+
+	document.addEventListener('mouseup', function(e){
+		function closePopupsOrTooltips($items, _parentEl, _oneEl){
+			$items.forEach(function($item, indx){
+				if (_oneEl === undefined && $item.contains(e.target) === false){
+					$item.closest(_parentEl).classList.remove('_active');
+				} else if ($item.contains(e.target) === false && $item.closest(_parentEl).querySelector(_oneEl).contains(e.target) === false){
+					$item.closest(_parentEl).classList.remove('_active');
+				}
+			})
 		}
-		scrollPos = st;
-		if (st > 10) {
-			$('.header').addClass('scroll');
-		} else {
-			$('.header').removeClass('scroll');
-		}
-	});
 
-	gsap.to(gallery_Wrapper, {
-		scrollTrigger: {
-			trigger: '.gallery',
-			start: '0% 0%',
-			end: 'bottom bottom',
-			pinSpacing: false,
-			scrub: true,
-			markers: true,
-			// markers: { startColor: "blue", endColor: "red", fontSize: "18px", fontWeight: "bold", indent: 400 },
-			// fastScrollEnd: 3000,
-			// pin: true,
-			// id: 'image',
-			// toggleActions: 'pause',
-			onEnter: () => {
-			},
-			onEnterBack: () => {
-
-			},//Вернусля в блок
-			onLeave: () => {
-			},//Покинул блок
-			onLeaveBack: () => {
-			},//Вернулся в покинувший блок
-		},
-	});
-
-	SmoothScroll({
-		// Время скролла 400 = 0.4 секунды
-		animationTime: 900,
-		// Размер шага в пикселях
-		stepSize: 100,
-
-		// Дополнительные настройки:
-
-		// Ускорение
-		accelerationDelta: 130,
-		// Максимальное ускорение
-		accelerationMax: 1,
-
-		// Поддержка клавиатуры
-		keyboardSupport: true,
-		// Шаг скролла стрелками на клавиатуре в пикселях
-		arrowScroll: 50,
-
-		// Pulse (less tweakable)
-		// ratio of "tail" to "acceleration"
-		pulseAlgorithm: true,
-		pulseScale: 4,
-		pulseNormalize: 1,
-
-		// Поддержка тачпада
-		touchpadSupport: true,
+		closePopupsOrTooltips(document.querySelectorAll('.tooltip__body'), '.tooltip', '.tooltip__head');
+		closePopupsOrTooltips(document.querySelectorAll('.submenu__list'), '.submenu', '.submenu__js>a');
 	})
-}
 
-
+	function getSiblings(elem) {
+		let siblings = [];
+		let sibling = elem;
+		while (sibling.previousSibling) {
+			sibling = sibling.previousSibling; sibling.nodeType == 1 && siblings.push(sibling);
+		}
+		sibling = elem;
+		while (sibling.nextSibling) {
+		sibling = sibling.nextSibling; sibling.nodeType == 1 && siblings.push(sibling);
+		} return siblings;
+	}
+})
